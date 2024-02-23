d2b0e705cf309b558276179f3f26234956e1543b
sun-28 <sunpahwa28@gmail.com>
2024-02-23 18:36:13 +0530

    reverse proxy added

---
+++ server/rev-proxy/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const httpProxy = require('http-proxy')
+require('dotenv').config()
+
+const app = express();
+
+const BaseURL = process.env.BASE_URL
+const proxy = httpProxy.createProxy()
+
+app.use((req,res)=>{
+
+    const hostname = req.hostname;
+
+    const sub = hostname.split('.')[0];
+
+    const resolvesTo = `${BaseURL}/${sub}`
+
+    return proxy.web(req, res, { target: resolvesTo, changeOrigin: true })
+
+})
+
+
+proxy.on('proxyReq', (proxyReq, req, res) => {
+    const url = req.url;
+    if (url === '/')
+        proxyReq.path += 'index.html'
+
+})
+
+app.listen(5000,()=>{
+    console.log('Server running on PORT : 5000')
+})
+
