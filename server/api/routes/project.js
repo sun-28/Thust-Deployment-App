71de99ea74e2c2bb37fd94328cd7309d906049de
sun-28 <sunpahwa28@gmail.com>
2024-02-25 23:12:07 +0530

    delete endpoint updated

--- server/api/routes/project.js
+++ server/api/routes/project.js
@@ -1,9 +1,17 @@
 const router = require("express").Router();
 const { ECSClient, RunTaskCommand } = require("@aws-sdk/client-ecs");
+const { S3Client, DeleteObjectsCommand , ListObjectsV2Command } = require("@aws-sdk/client-s3");
 const TProject = require("../models/TProject");
 const authenticate = require("../middleware/authenticate");
 
 const ecsClient = new ECSClient({
+  region: "ap-south-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
+const s3Client = new S3Client({
   region: "ap-south-1",
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY,
@@ -15,6 +23,39 @@ const config = {
   CLUSTER: process.env.CLUSTER,
   TASK: process.env.TASK,
 };
+
+
+
+async function deleteFolder(slug) {
+  async function recursiveDelete(token) {
+    const listCommand = new ListObjectsV2Command({
+      Bucket: "thrust-deployment", 
+      Prefix: `builds/${slug}`,
+      ContinuationToken: token
+    });
+    let list = await s3Client.send(listCommand);
+    if (list.KeyCount) {
+      const deleteCommand = new DeleteObjectsCommand({
+        Bucket: "thrust-deployment",
+        Delete: {
+          Objects: list.Contents.map((item) => ({ Key: item.Key })),
+          Quiet: false,
+        },
+      });
+      let deleted = await s3Client.send(deleteCommand);
+      if (deleted.Errors) {
+        deleted.Errors.map((error) => console.log(`${error.Key} could not be deleted - ${error.Code}`));
+      }
+    }
+    if (list.NextContinuationToken) {
+      recursiveDelete(list.NextContinuationToken);
+    }
+  };
+  return recursiveDelete();
+};
+
+
+
 
 router.post("/deploy", authenticate, async (req, res) => {
   try {
@@ -138,7 +179,7 @@ router.get("/getprojects", authenticate, async (req, res) => {
   }
 });
 
-router.delete("/deletepoject", authenticate, async (req, res) => {
+router.delete("/deleteproject", authenticate, async (req, res) => {
   try {
     const userId = req.userId;
     const { slug } = req.body;
@@ -148,6 +189,9 @@ router.delete("/deletepoject", authenticate, async (req, res) => {
     if (!project) {
       return res.json({ success: false, error: "Project Not Found" });
     }
+
+    await deleteFolder(slug);
+
     res.json({ success: true, msg: "Project Deleted" });
   } catch (error) {
     console.log(error);
